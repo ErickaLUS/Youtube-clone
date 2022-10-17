98447fa455fa38776497b638a364cecfda2e2fbc
ErickaL2 <106146557+ErickaLUS@users.noreply.github.com>
2022-10-17 16:45:44 +0000

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css'
-import Search from './Search'
+
 
 export default function App() {
   return (
     <main>
-        <Search/>
+      vdvdvdv
     </main>
   )
 }
--- src/Search.jsx
+++
@@ -1,8 +0,0 @@
- const Search = () => {
-  return(
-    <div>
-        <input type="text" name="search" placeholder="recherche" className="search"/>
-    </div>
-  )
-}
-export default Search
