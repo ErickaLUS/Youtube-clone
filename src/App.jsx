0bbe61d4d3c94c081ba99a150f0b7f02645b899b
ericka lus <lusericka0@gmail.com>
2022-10-18 11:14:23 +0100

    ajout fonction authentification vidéos populaires liste des chaines abonné

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,89 @@
-import './App.css'
+import { children, useState, useEffect } from "react";
+import styled from "styled-components";
+import { ThemeProvider } from "styled-components";
+import Menu from "./components/Menu";
+import Navbar from "./components/Navbar";
+import { darkTheme, lightTheme } from "./components/utils/Theme";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Videos from "./pages/Videos.jsx";
+import SignIn from "./pages/SignIn.jsx";
+import Subscription from "./pages/Subscription.jsx";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
 
+const Container = styled.div`
+  display: flex;
+`;
+
+const Main = styled.div`
+  flex: 7;
+  background-color: ${({ theme }) => theme.bg};
+`;
+
+const Wrapper = styled.div``;
+const Layout = ({ children }) => {
+  const [darkMode, setDarkMode] = useState(true);
+  return (
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+      <Container className="App">
+        <Menu darkMode={darkMode} setDarkMode={setDarkMode} />
+        <Main>
+          <Navbar />
+          <Wrapper>{children}</Wrapper>
+        </Main>
+      </Container>
+    </ThemeProvider>
+  );
+};
 
 export default function App() {
+  const accessToken = useSelector((state) => state.auth.accessToken);
+  
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!accessToken) {
+      navigate("/SignIn");
+    }
+  }, [accessToken]);
+  console.log(accessToken);
   return (
-    <main>
-      vdvdvdv
-    </main>
-  )
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <Layout>
+            <Home />
+          </Layout>
+        }
+      />
+      <Route path="/SignIn" element={<SignIn />} />
+      <Route
+        path="/videos"
+        element={
+          <Layout>
+            <Videos />
+          </Layout>
+        }
+      />
+      <Route
+        path="/videos/:id"
+        element={
+          <Layout>
+            <Videos />
+          </Layout>
+        }
+      />
+      <Route
+        path="/subscription"
+        element={
+          <Layout>
+            <Subscription />
+          </Layout>
+        }
+      />
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  );
 }
---
+++ src/pages/Subscription.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import styled from "@emotion/styled";
+import { useDispatch, useSelector } from "react-redux";
+import { getSubscribedChannels } from "../redux/actions/videos.action";
+import { useEffect } from "react";
+import Cards from "../components/Cards";
+import { Link } from "react-router-dom";
+import { style } from "@mui/system";
+
+const Container = styled.div``;
+const Hr = styled.hr`
+  margin-left: 0px;
+  border: 3px solid ${({ theme }) => theme.soft};
+`;
+const Title = styled.div``;
+const Content = styled.div``;
+const Image = styled.img`
+  width: 24%;
+  height: 7%;
+  background-color: #9999;
+`;
+const Description = styled.div`
+  margin-left: 35px;
+`;
+
+const Subscription = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getSubscribedChannels());
+  }, [dispatch]);
+
+  const { videos } = useSelector((state) => state.subscriptionsChannel);
+  console.log(videos);
+
+  return (
+    <Container>
+      {videos?.map((video) => (
+        <div key={video.id}>
+          <Image src={video?.snippet?.thumbnails?.medium?.url} alt="tg" />
+
+          <div key={video.id}>{video.snippet.title}</div>
+          <Description>{video.snippet.description}</Description>
+        </div>
+      ))}
+    </Container>
+  );
+};
+
+export default Subscription;
